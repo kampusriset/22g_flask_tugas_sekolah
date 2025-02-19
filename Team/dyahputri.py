a84e098d984ada6e01477dd0cf612e7512d6a011
hdfrme <mm.diahputri@gmail.com>
2025-02-19 22:52:56 +0700

    Create dyahputri.py

---
+++ Team/dyahputri.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Dyah Putri Anggraini"
+    nim = "2213010385"
+    peran = "Mengerjakan semua fitur"
+    perkenalan(nama, nim, peran)
