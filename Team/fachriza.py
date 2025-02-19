2a48f91854f4576ba1b0377d990e30e8fc451986
fachriza03 <fachrizaulfah7@gmail.com>
2025-02-19 22:43:53 +0700

    fachriza.py

---
+++ Team/fachriza.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if _name_ == "_main_":
+    nama = "Fachriza Ulfah"
+    nim = "2213010396"
+    peran = "Mengerjakan semua fitur"
+    perkenalan(nama, nim, peran)
